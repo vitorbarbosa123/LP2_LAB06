3c0c8d60b059ed9bf2797e00c3fcb20fb38a1616
hernandes-matheus <hernandes.ma.silva@gmail.com>
2022-08-07 22:29:17 -0300

    feat(Tarefa): implementando as funcionalidade de tarefas no FacadeApp TarefasController, e construindo Tarefas

--- LAB06/SAPO/src/sapo/FacadeApp.java
+++ LAB06/SAPO/src/sapo/FacadeApp.java
@@ -20,4 +20,54 @@ public class FacadeApp {
 
     }
 
+
+
+    // todo: mudar futuramente
+    public String cadastrarTarefa(String atividadeId, String nome, String[] habilidades) {
+        int finalDoIDTarefa = this.atividadesController.getQuantidadeTaferas(atividadeId);
+        String tarefaID = atividadeId+"-"+finalDoIDTarefa;
+
+        Tarefas novaTarefa =  this.tarefasController.cadastrarTarefa(tarefaID, nome, habilidades);
+        this.atividadesController.cadastrarTarefa(atividadeId, novaTarefa);
+
+        return tarefaID;
+    }
+
+    public void alterarNomeTarefa(String idTarefa, String novoNome) {
+        this.tarefasController.alterarNomeTarefa(idTarefa, novoNome);
+    }
+
+    public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
+        this.tarefasController.alterarHabilidadesTarefa(idTarefa, habilidades);
+    }
+
+    public void adicionarHorasTarefa(String idTarefa, int horas) {
+        this.tarefasController.adicionarHorasTarefa(idTarefa, horas);
+    }
+
+    public void removerHorasTarefa(String idTarefa, int horas) {
+        this.tarefasController.removerHorasTarefa(idTarefa, horas);
+    }
+
+    public void concluirTarefa(String idTarefa) {
+        this.tarefasController.concluirTarefa(idTarefa);
+    }
+
+    public void removerTarefa(String idTarefa) {
+        this.tarefasController.removerTarefa(idTarefa);
+    }
+
+    public String exibirTarefa(String idTarefa) {
+        this.tarefasController.exibirTarefa(idTarefa)
+    }
+    
+    public void associarPessoaTarefa(String cpf, String idTarefa) {
+       //Pessoa pessoa = this.pessoasController.recuperarPessoa(cpf);
+       //this.tarefasController.associarPessoaTarefa(cpf, idTarefa, pessoa);
+    }  
+
+    public void removerPessoaTarefa(String cpf, String idTarefa) {
+        this.tarefasController.removerPessoaTarefa(cpf, idTarefa);
+    } 
+
 }
--- LAB06/SAPO/src/sapo/TarefasController.java
+++ LAB06/SAPO/src/sapo/TarefasController.java
@@ -4,30 +4,37 @@ import java.util.*;
 
 public class TarefasController {
 
-    private HashMap<str, Tarefas> tarefas;
+    private HashMap<String, Tarefas> tarefas;
 
-    public String cadastrarTarefa(String atividadeId, String nome, str[] habilidades) {
-
+    public Tarefas cadastrarTarefa(String tarefaID, String nome, String[] habilidades) {
+        Tarefas novaTarefa = new Tarefas(tarefaID, nome, habilidades);
+        this.tarefas.put(tarefaID, novaTarefa);
+        return novaTarefa;
     }
 
     public void alterarNomeTarefa(String idTarefa, String novoNome) {
-
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        tarefa.setNome(novoNome);
     }
 
-    public void alterarHabilidadesTarefa(String idTarefa, str[] habilidades) {
-
+    public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        tarefa.setHabilidades(habilidades);
     }
 
     public void adicionarHorasTarefa(String idTarefa, int horas) {
-
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        tarefa.adicionarHoras(horas);
     }
 
     public void removerHorasTarefa(String idTarefa, int horas) {
-
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        tarefa.removerHoras(horas);
     }
 
     public void concluirTarefa(String idTarefa) {
-
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        tarefa.concluirTarefa();
     }
 
     public void removerTarefa(String idTarefa) {
@@ -35,15 +42,23 @@ public class TarefasController {
     }
 
     public String exibirTarefa(String idTarefa) {
-
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        return tarefa.toString();
     }
 
-    public void associarPessoaTarefa(String cpf, String idTarefa) {
-
+    public void associarPessoaTarefa(String cpf, String idTarefa, Pessoa pessoa) {
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        tarefa.adicionarNovoResponsavel(cpf, pessoa);
     }
 
     public void removerPessoaTarefa(String cpf, String idTarefa) {
-
+        Tarefas tarefa =this.recuperarTarefaOrException(idTarefa);
+        tarefa.removerResponsavel(cpf);
     }
 
+
+    private Tarefas recuperarTarefaOrException(String idTarefa){
+        if(tarefas.containsKey(idTarefa)) return tarefas.get(idTarefa);
+        throw new NoSuchElementException("Tarefa não existe");
+    }
 }
