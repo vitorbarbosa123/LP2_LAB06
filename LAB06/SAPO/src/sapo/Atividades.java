18c47fefa6dcb7daee0abf92fa832de4d6ada904
vitor barbosa <80813598@redecorp.br>
2022-08-14 10:35:15 -0300

    Modificacoes em atividade

--- LAB06/SAPO/src/sapo/Atividades.java
+++ LAB06/SAPO/src/sapo/Atividades.java
@@ -1,5 +1,8 @@
 package sapo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.List;
 
 public class Atividades {
@@ -9,19 +12,28 @@ public class Atividades {
 
     private String nome;
 
-    private str[] habilidades;
-
-    private String novoNome;
-
-    private str[] novasHabilidades;
-
-    private String comentario;
-
     private String autorCpf;
+    
+    private String descricao;
 
-    private List<Tarefas> tarefas;
-
-
+    private ArrayList<Tarefas> tarefas;
+    
+    private boolean concluida;
+    
+ 
+    public Atividades(String nome, String descricao, String cpf) {
+    	this.nome = nome;
+    	this.descricao = descricao;
+    	this.autorCpf = cpf;
+    	this.concluida = false;
+    	
+    	
+    	if(cpf.isEmpty() || cpf.isBlank()  ) {
+    		throw new IllegalArgumentException("CPF VAZIO OU NULO");
+    	}
+    }
+    
+          
     public int getQuantidadeTarefas(){
         return this.tarefas.size();
     }
@@ -29,4 +41,58 @@ public class Atividades {
     public void cadastrarTarefa(Tarefas novaTarefa){
         tarefas.add(novaTarefa);
     }
+
+
+	public void alterarDescricaoAtividade(String novaDescricao) {
+		this.descricao = novaDescricao;
+	}
+
+
+	public void alterarResponsavelAtividade(String novoCpf) {
+		this.autorCpf = novoCpf;
+	}
+
+
+	public String exibirAtividade(String codigo) {
+		
+		
+		return codigo + ":" + this.nome
+				+ "Responsável: Matheus Gaudencio do Rêgo – " + cpf
+				+ "==="
+				+ this.descricao
+				+ "==="
+				+ "Tarefas:" + tarefasRealizadas + "/" + tarefasCadastradas
+				+ "-" +  tarefas.get(0)
+				+ "-" +  tarefas.get(1)
+				+ "-" +  tarefas.get(2);
+		
+	}
+
+
+	public void concluirAtividade() {
+        this.concluida = true;
+    }
+	
+	public void encerrarAtividade(String atividadeId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void reabrirAtividade(String codigo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void desativarAtividade(String codigo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	
 }
