31de6d18dbb6f573d7ec488ac45a603d54867966
Daniella Dantas <daniellastdt@gmail.com>
2022-08-14 11:15:51 -0300

    atualização da brench

---
+++ LAB06/SAPO/src/sapo/Facade.java
@@ -0,0 +1,111 @@
+package sapo;
+
+public class Facade {
+
+    private PessoasController pessoasController;
+    private AtividadesController atividadesController;
+    private TarefasController tarefasController;
+
+    public Facade() {
+        this.pessoasController = new PessoasController();
+        this.atividadesController = new AtividadesController();
+        this.tarefasController = new TarefasController();
+    }
+
+    public PessoasController getPessoasController() {
+        return pessoasController;
+    }
+
+    public AtividadesController getAtividadesController() {
+        return atividadesController;
+    }
+
+    public TarefasController getTarefasController() {
+        return tarefasController;
+    }
+
+    public void cadastrarPessoa(String cpf, String nome, String[] habilidades) {
+        this.pessoasController.cadastrarPessoa(cpf, nome, habilidades);
+    }
+
+    public Pessoa buscarPessoa(String cpf) {
+        return this.pessoasController.buscarPessoa(cpf);
+    }
+
+    public String exibirPessoa(String cpf) {
+        return this.pessoasController.exibirPessoa(cpf);
+    }
+
+    public void alterarNomePessoa(String cpf, String novoNome) {
+        this.pessoasController.alterarNomePessoa(cpf, novoNome);
+    }
+
+    public void alterarHabilidadesPessoa(String cpf, String[] novasHabilidades) {
+        this.pessoasController.alterarHabilidadesPessoa(cpf, novasHabilidades);
+    }
+
+    public void removerPessoa(String cpf) {
+        this.pessoasController.removerPessoa(cpf);
+    }
+
+    public void adicionarComentarioPessoa(String cpf, String comentario, String autorCpf) {
+        this.pessoasController.adicionarComentarioPessoa(cpf, comentario, autorCpf);
+    }
+
+    public String listarComentariosPessoa(String cpf) {
+        return this.pessoasController.listarComentariosPessoa(cpf);
+    }
+
+    Pessoa recuperarPessoaOrException(String cpf){
+        return this.pessoasController.recuperarPessoaOrException(cpf);
+    }
+
+    // todo: mudar futuramente
+    public String cadastrarTarefa(String atividadeId, String nome, String[] habilidades) {
+        int finalDoIDTarefa = this.atividadesController.getQuantidadeTaferas(atividadeId);
+        String tarefaID = atividadeId + "-" + finalDoIDTarefa;
+
+        Tarefas novaTarefa =  this.tarefasController.cadastrarTarefa(tarefaID, nome, habilidades);
+        this.atividadesController.cadastrarTarefa(atividadeId, novaTarefa);
+
+        return tarefaID;
+    }
+
+    public void alterarNomeTarefa(String idTarefa, String novoNome) {
+        this.tarefasController.alterarNomeTarefa(idTarefa, novoNome);
+    }
+
+    public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
+        this.tarefasController.alterarHabilidadesTarefa(idTarefa, habilidades);
+    }
+
+    public void adicionarHorasTarefa(String idTarefa, int horas) {
+        this.tarefasController.adicionarHorasTarefa(idTarefa, horas);
+    }
+
+    public void removerHorasTarefa(String idTarefa, int horas) {
+        this.tarefasController.removerHorasTarefa(idTarefa, horas);
+    }
+
+    public void concluirTarefa(String idTarefa) {
+        this.tarefasController.concluirTarefa(idTarefa);
+    }
+
+    public void removerTarefa(String idTarefa) {
+        this.tarefasController.removerTarefa(idTarefa);
+    }
+
+    public String exibirTarefa(String idTarefa) {
+        return this.tarefasController.exibirTarefa(idTarefa);
+    }
+    
+    public void associarPessoaTarefa(String cpf, String idTarefa) {
+       Pessoa pessoa = this.pessoasController.recuperarPessoaOrException(cpf);
+       this.tarefasController.associarPessoaTarefa(cpf, idTarefa, pessoa);
+    }  
+
+    public void removerPessoaTarefa(String cpf, String idTarefa) {
+        this.tarefasController.removerPessoaTarefa(cpf, idTarefa);
+    } 
+
+}
